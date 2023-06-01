87b77fa2b5d944fb634c54196cc707f90b5fac41
awressy <awressy@gmail.com>
2023-06-01 20:06:04 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server berjalan di http://0.0.0.0:${port}`);
+});
