5eb0c89919dce3775fb3e9f4fe341871d0b44ad4
awressy <awressy@gmail.com>
2023-07-08 21:03:18 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 // Contoh data
 let books = [
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server berjalan di http://0.0.0.0:${port}`);
